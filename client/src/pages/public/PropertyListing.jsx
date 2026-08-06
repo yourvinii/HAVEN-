51c7b8c11ff2a24ffbbf3c4c1bc1b1bb30fe6dce
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-08-06 20:52:43 +0530

    feat: Folder Structure and Foundation with each file empty placeholder

---
+++ client/src/pages/public/PropertyListing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PropertyListing = () => {
+  return <div>PropertyListing</div>;
+};
+
+export default PropertyListing;
---
+++ client/src/pages/seller/MyProperties.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MyProperties = () => {
+  return <div>MyProperties</div>;
+};
+
+export default MyProperties;
+
+
