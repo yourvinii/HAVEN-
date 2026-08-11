96efd2718c2986d8aa2d2f9932f06dc2f99d0152
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-08-11 17:53:04 +0530

    removed /me, /tenant-test, /owner-test, /admin-test

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -13,49 +13,5 @@ router.route("/verify-otp").post(verifyOTP);
 router.route("/resend-otp").post(resendOTP);
 router.route("/login").post(loginUser);
 
-// temporary protected routes
-import protect from "../middlewares/authMiddleware.js";
-router.route("/me").get(protect, (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Your are authencicated",
-    user: req.user,
-  });
-});
-
-// Temporary role test route
-import authorizeRole from "../middlewares/roleMiddleware.js";
-// tenant
-router
-  .route("/tenant-test")
-  .get(protect, authorizeRole("tenant"), (req, res) => {
-    res.status(200).json({
-      success: true,
-      message: "Tenant access guranted",
-      user: req.user,
-    });
-  });
-
-// owner
-router
-  .route("/owner-test")
-  .get(protect, authorizeRole("owner"), (req, res) => {
-    res.status(200).json({
-      success: true,
-      message: "owner access guranted",
-      user: req.user,
-    });
-  });
-
-// admin
-router
-  .route("/admin-test")
-  .get(protect, authorizeRole("admin"), (req, res) => {
-    res.status(200).json({
-      success: true,
-      message: "admin access guranted",
-      user: req.user,
-    });
-  });
 
 export default router;
