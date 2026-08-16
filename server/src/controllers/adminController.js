c7057f59cd155c6fc300cb9268b01773ecc748d7
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-08-16 19:44:31 +0530

    Admin Delete Properties

--- server/src/controllers/adminController.js
+++ server/src/controllers/adminController.js
@@ -44,4 +44,32 @@ const getAllProperties = async (req, res) => {
   }
 };
 
-export { getAllUsers, getAllProperties };
+const deleteProperty = async (req, res) => {
+  try {
+    const { propertyId } = req.params;
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) {
+      return res.status(404).json({
+        success: false,
+        message: "Property not found",
+      });
+    }
+
+    await Property.findByIdAndDelete(propertyId);
+
+    return res.status(200).json({
+      success: true,
+      message: "Property deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to delete property",
+      error: error.message,
+    });
+  }
+};
+
+export { getAllUsers, getAllProperties, deleteProperty };
--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import protect from "../middlewares/authMiddleware.js";
 import authorizeRoles from "../middlewares/roleMiddleware.js";
 import {
+  deleteProperty,
   getAllProperties,
   getAllUsers,
 } from "../controllers/adminController.js";
@@ -21,5 +22,9 @@ adminRouter.route("/users").get(protect, authorizeRoles("admin"), getAllUsers);
 adminRouter
   .route("/properties")
   .get(protect, authorizeRoles("admin"), getAllProperties);
+
+adminRouter
+  .route("/:propertyId")
+  .delete(protect, authorizeRoles("admin"), deleteProperty);
 
 export default adminRouter;
