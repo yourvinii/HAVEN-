ee64be13d31c7868aaec68ae3bcde1b0a30fd839
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-08-21 21:10:02 +0530

    socket http server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,16 @@
+import http from "http";
+
 import app from "./app.js";
 import connectDB from "./config/db.js";
+
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(8080, () => {
+    const server = http.createServer(app);
+
+    
+    server.listen(8080, () => {
       console.log("Server is running on http://localhost:8080");
     });
   } catch (error) {
