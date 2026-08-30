74ff669d4225d03bb5c04cb13f1f507033fb9522
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-08-30 14:18:27 +0530

    attach JWT automatically in Axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getToken } from "../utils/storage";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -6,5 +7,20 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
