82b71f972082ce739bdf6f3b182f7cd17c83e208
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-08-30 14:05:06 +0530

    tailwind installation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>KDRent</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold ">KDRent</h1>
       <p>Rental Property Management</p>
     </div>
   );
