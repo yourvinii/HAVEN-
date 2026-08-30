2758b53195bcc326fd37f2ca4bebf9fcf14bf3b8
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-08-30 13:58:38 +0530

    Frontend Folder Structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>KDRent</h1>
+      <p>Rental Property Management</p>
+    </div>
+  );
+};
+
+export default App;
