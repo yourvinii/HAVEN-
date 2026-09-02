264edd6e750fc83b8dac15ef3ad4c60d70655981
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-09-02 19:54:02 +0530

    dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        KDRent Dashboard
+      </h1>
+    </div>
+  );
+}
+
+export default Dashboard;
