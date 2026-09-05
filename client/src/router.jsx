f0035d85731574aa30a0ffc6cd057987b1d63609
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-09-05 14:32:06 +0530

    Client Folder Setup

---
+++ client/src/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1 className="text-4xl text-gray-400 bg-red-50 ">Hello</h1>,
+  },
+]);
+
+export default router;
