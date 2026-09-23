8c98decb555c5be480e5eef44cd06f4a18e8001c
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-09-23 20:41:17 +0530

    feat: modified main & router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,12 @@ import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
+import { AuthProvider } from "./features/auth/auth.context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,12 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./features/homePage/HomePage";
+import Login from "./features/auth/pages/Login";
+import Register from "./features/auth/pages/Register";
+import DemoCredentials from "./features/auth/pages/DemoCredentials";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
-  
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path:"/demo-login",
+    element:<DemoCredentials/>
+  }
 ]);
 
 export default router;
